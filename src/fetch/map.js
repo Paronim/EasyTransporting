177752d00bd83c2b43b096277305e6a015b7b5c6
Paronim <Zreba.v@gmial.com>
2023-11-12 09:55:53 +0300

    last

--- src/fetch/map.js
+++ src/fetch/map.js
@@ -51,7 +51,7 @@ export async function elevation(staytions) {
 
   try {
     let routing = await fetch(
-      `https://overpass-api.de/api/interpreter?data=%2F*%0AThis%20query%20looks%20for%20nodes%2C%20ways%20and%20relations%20%0Awith%20the%20given%20key%2Fvalue%20combination.%0AChoose%20your%20region%20and%20hit%20the%20Run%20button%20above%21%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Bnwr%5Brailway%3Drail%5D%28${dataСoordinates}%29%3B%28node%28around%3A1000%2C%2052.2924%2C35.398%29%3Bnode%28around%3A1000%2C%2052.2727%2C35.4253%29%3B%29%3Bnwr%28around%3A1000%29%5B%22railway%22%3D%22rail%22%5D%3B%28._%3B%3E%3B%29%3Bout%3B`
+      `https://overpass-api.de/api/interpreter?data=%2F*%0AThis%20query%20looks%20for%20nodes%2C%20ways%20and%20relations%20%0Awith%20the%20given%20key%2Fvalue%20combination.%0AChoose%20your%20region%20and%20hit%20the%20Run%20button%20above%21%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Bnwr%5Brailway%3Drail%5D%28${dataСoordinates}%29%3B%28node%28around%3A7000%2C%2052.2924%2C35.398%29%3Bnode%28around%3A7000%2C%2052.2727%2C35.4253%29%3B%29%3Bnwr%28around%3A7000%29%5B%22railway%22%3D%22rail%22%5D%3B%28._%3B%3E%3B%29%3Bout%3B`
     ).then(async (result) => {
       result = await result.json();
       console.log(result);
