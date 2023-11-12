7e2fa515b8321e180fc69efdfbaf2bd37a94a65f
Paronim <Zreba.v@gmial.com>
2023-11-12 10:01:49 +0300

    e

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
   <main id="main">
     <LeftBlock @open="openRightBlock" />
     <Suspense>
-      <!-- <componentMap @openRightBlock="openRightBlockMarker()" /> -->
-      <MapComponent />
+      <componentMap @openRightBlock="openRightBlockMarker()" />
     </Suspense>
     <RightBlock v-if="showRightBlock" @close="showRightBlock = false" />
     <RightBlockMarker
@@ -21,12 +20,11 @@ import LeftBlock from "./components/LeftBlock.vue";
 import RightBlock from "./components/RightBlock.vue";
 import RightBlockMarker from "./components/RightBlockMarker.vue";
 import HeaderComponent from "./components/Header/HeaderComponent.vue";
-// import componentMap from "./components/componentMap.vue";
+import componentMap from "./components/componentMap.vue";
 import PopupSearch from "./components/Popup/PopupSearch.vue";
 
 import { ref } from "vue";
 import NewNotifications from "@/components/NewNotifications.vue";
-import MapComponent from "@/components/MapComponent.vue";
 
 const showRightBlock = ref(false);
 const showRightBlockMarker = ref(false);
@@ -37,10 +35,10 @@ const openRightBlock = () => {
   showRightBlockMarker.value = false;
 };
 
-// const openRightBlockMarker = () => {
-//   showRightBlockMarker.value = true;
-//   showRightBlock.value = false;
-// };
+const openRightBlockMarker = () => {
+  showRightBlockMarker.value = true;
+  showRightBlock.value = false;
+};
 </script>
 
 <style lang="scss">
